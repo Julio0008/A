50d0b3ff1f1561c148fa299519d29ff7dd55b68b
Julio0008 <julion.s.nowak@eskymos.com>
2025-04-23 12:19:53 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let score = 0;
-const button = document.getElementById('clickBtn');
-const scoreDisplay = document.getElementById('score');
-
-button.addEventListener('click', () => {
-  score++;
-  scoreDisplay.textContent = score;
-});
